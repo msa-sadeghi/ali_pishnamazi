b929e6f8ea2514cd2dedebdadbe2ea4810d60ac6
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-10-02 18:59:21 +0330

    a

--- javascript/session1/script.js
+++ javascript/session1/script.js
@@ -1,46 +1,59 @@
-// var x = 0;
-// var y = ++x;
-// console.log(y)
-// console.log(x)
+// var i = 0;
 
-// for(let i = 0; i < 3; i++){
-//     console.log(`${i}- hi`)
+// while(i < 5){
+//     console.log(`i${i}`)
+//     i++;
 // }
 
-// var name = 'nikan';
-// for(let i = 0; i < name.length; i++){
-//     console.log(`${name[i]}`)
+// do{
+//     console.log(`i${i}`)
+//     i++;
+// }while(i<5)
+
+// var array = [1,2,3]
+// console.log(Array.isArray(array))
+// array.push('ali')
+// console.log(array)
+// console.log(array.length)
+// console.log(array[0])
+// for(var i = 0; i < array.length; i++){
+//     console.log(array[i])
+// }
+// array.pop()
+// array.forEach(function(item){
+//     console.log(item)
+// })
+
+// var array = [1,2,3]
+// array.shift()
+// array.unshift(123,222)
+// console.log(array)
+var array = [1,2,3,4]
+// array.splice(1,0,222)
+// console.log(array)
+// var newArray = [
+//     ...array.slice(0,1),
+//     1234,22222, ...array.slice(1)
+// ]
+
+
+// console.log(newArray)
+
+
+// array.forEach(function(a){
+//     console.log(arguments)
+//     // console.log(args)
+// })
+
+
+// function add(a,e,...b){
+//     // console.log(arguments[0])
+//     console.log(a)
+//     console.log(b)
 // }
 
-// اعداد زوج بین 0 تا 100 را در کنسول چاپ کنید
+// add(1,2,3,4,5)
 
 
-// فرض کنید یک وب سایت فروشگاهی دارید
-//  که کاربر 5 محصول از فروشگاه را انتخاب کرده است
 
-// قیمت 5 محصول را از کاربر گرفته
-//  و مجموع مبلغ کل سبد خرید رو نمایش دهید
-
-
-// عددی را از کاربر دریافت کرده و سپس مجموع رقم های آن را محاسبه کنید.
-
-// :به عنوان مثال برای عدد 142 به صورت زیر محاسبه می شود
-
-// 1 + 4 + 2 = 7
-
-var x = 0
-var a;
-var y = true;
-for(var i = 0; i < 5; i++){
-    y = true
-        for(var j = 0; y == true; j++){
-            a = +prompt("enter a number")
-            if (!isNaN(a)){
-                y = false
-            }
-        }
-         
-         x += a
-
-}
-console.log(x)
+array.forEach((i) => console.log(`item#${i}`))
